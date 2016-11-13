2855fe1ac9102ce9d9ba592d5ebadda90e6717b5
zhazha <1208484996@qq.com>
2016-11-13 15:03:55 +0800

    postcss-config

--- config/postcss/index.js
+++ config/postcss/index.js
@@ -3,41 +3,22 @@
 const webpack = require('webpack');
 const path = require('path')
 const postcssBasePlugins = [
-  require('precss'),
-  require('postcss-css-variables')(),
-  require("postcss-color-function")(),
-  require('postcss-modules-local-by-default'),
-  require('postcss-import')({
-    addDependencyTo: webpack,
-    // path: [path.resolve(__dirname, '../../src')],
-  }),
-    // require('postcss-smart-import')(),
-  require('postcss-cssnext')({
-        features: {
-          customProperties: {
-           preserve: true
-          }
-        }
+  require("postcss-import")({ addDependencyTo: webpack }),
+      require("postcss-url")(),
+      require("postcss-cssnext")({
+        browsers:'Last 2 versions Safari >= 8 IE >= 9'
       }),
-  require('postcss-browser-reporter')(),
-      require('postcss-reporter')(),
+      // add your "plugins" here
+      require('postcss-custom-properties')(),
+      require("postcss-color-function")(),
+      // and if you want to compress,
+      // just use css-loader option that already use cssnano under the hood
+      require("postcss-browser-reporter")(),
+      require("postcss-reporter")(),
 ];
 const postcssDevPlugins = [];
 const postcssProdPlugins = [
-  require("postcss-custom-properties")(),
-  // require('autoprefixer')({
-  //     browsers: [
-  //         '>1%',
-  //         'last 4 versions',
-  //         'Firefox ESR',
-  //         'not ie < 9',
-  //        ]
-  //   }), 
-  // require('cssnano')({
-  //   safe: true,
-  //   sourcemap: true,
-  //   autoprefixer: false,
-  // }),
+
 ];
 
 const postcssPlugins = postcssBasePlugins
